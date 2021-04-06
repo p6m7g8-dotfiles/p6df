0871f307e5aa453f3e5db436b498092cc808d7a2
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-05 21:01:28 -0400

    chore: add docker_test (#4)

---
+++ init.zsh
@@ -0,0 +1,16 @@
+
+######################################################################
+#<
+#
+# Function: p6df::modules::p6ctl::init()
+#
+#  Depends:	 p6_bootstrap
+#  Environment:	 P6_DFZ_SRC_P6M7G8_DIR
+#>
+######################################################################
+p6df::modules::p6ctl::init() {
+
+  local dir="$P6_DFZ_SRC_P6M7G8_DIR/p6ctl"
+
+  p6_bootstrap "$dir"
+}
