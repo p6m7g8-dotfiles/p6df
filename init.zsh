dcf411bef060b99303192bd38482ec0ed9be1ab6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-08-10 15:51:16 -0400

    style: p6ify

--- init.zsh
+++ init.zsh
@@ -1,16 +1,16 @@
-
+# shellcheck shell=bash
 ######################################################################
 #<
 #
 # Function: p6df::modules::p6ctl::init()
 #
-#  Depends:	 p6_bootstrap
-#  Environment:	 P6_DFZ_SRC_P6M7G8_DIR
+#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
 #>
 ######################################################################
 p6df::modules::p6ctl::init() {
 
   local dir="$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6ctl"
-
   p6_bootstrap "$dir"
+
+  p6_return_void
 }
